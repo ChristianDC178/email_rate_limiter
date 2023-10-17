536725b0dc9ef8c3a31d66a1dd8b723c211977bd
christiandc178 <christiandc_178@hotmail.com>
2023-10-17 01:16:05 -0300

    - code improvement

--- src/main/java/org/example/ratelimit/EmailRuleRateLimit.java
+++ src/main/java/org/example/ratelimit/EmailRuleRateLimit.java
@@ -18,6 +18,78 @@ public class EmailRuleRateLimit extends EmailRuleRateLimitBase {
         this.ruleType = ruleType;
     }
 
+    @Override
+    public boolean CanSendEmail(String userId) {
+
+        UserEmailLog eLog = this.emailLogs.get(userId);
+        if (eLog == null) {
+            return true;
+        }
+
+        if (IsRuleByMinute() && (System.currentTimeMillis() - eLog.getEmailSentDateTime()) < Constants.MINUTE_IN_MILLISECONDS && eLog.getEmailCounter() < emailThreshold) {
+            return true;
+        }
+
+        if (IsRuleByHour() && (System.currentTimeMillis() - eLog.getEmailSentDateTime()) < Constants.HOUR_IN_MILLISECONDS && eLog.getEmailCounter() < emailThreshold) {
+            return true;
+        }
+
+        if (IsRuleByDay() && (LocalDate.now().toEpochDay() > eLog.getEmailSentDateTime()) && eLog.getEmailCounter() < emailThreshold) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void RegisterEmailSent(String userId) {
+        long emailSentAt = IsRuleByDay() ? LocalDate.now().toEpochDay() : System.currentTimeMillis();
+        UserEmailLog eLog = this.emailLogs.get(userId);
+        if (eLog == null) {
+            eLog = new UserEmailLog(emailSentAt);
+            this.emailLogs.put(userId, eLog);
+        } else {
+            eLog.update(emailSentAt);
+        }
+    }
+
+    public void StartCleanScheduler() {
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+
+            synchronized (this.emailLogs) {
+
+                Iterator<Map.Entry<String, UserEmailLog>> iterator = this.emailLogs.entrySet().iterator();
+
+                System.out.println("@@@ Running Clean Scheduler - Initial size: " + this.emailLogs.size());
+
+                // Iterate through the HashMap and remove entries containing "Remove"
+                while (iterator.hasNext()) {
+
+                    UserEmailLog eLog = iterator.next().getValue();
+                    long result = System.currentTimeMillis() - eLog.getEmailSentDateTime();
+
+                    if (IsRuleByMinute() && (result > Constants.MINUTE_IN_MILLISECONDS)) {
+                        System.out.println("### ByMinute - Message Cleaned");
+                        iterator.remove();
+                    } else if (IsRuleByHour() && (result > Constants.HOUR_IN_MILLISECONDS)) {
+                        System.out.println("### ByHour - Message Cleaned");
+                        iterator.remove();
+                    } else if (IsRuleByDay() && (LocalDate.now().toEpochDay() > eLog.getEmailSentDateTime())) {
+                        System.out.println("### ByDay - Message Cleaned");
+                        iterator.remove();
+                    }
+
+                }
+
+                System.out.println("@@@ End of Clean Scheduler - Size: " + this.emailLogs.size());
+
+            }
+
+        }, 0, Constants.SCHEDULER_WAIT, translateToTimeUnit(ruleType));
+    }
+
     private TimeUnit translateToTimeUnit(RuleType ruleType) {
 
         if (ruleType == RuleType.ByMinute) {
@@ -32,80 +104,17 @@ public class EmailRuleRateLimit extends EmailRuleRateLimitBase {
 
     }
 
-    public void StartCleanScheduler() {
-
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
-        scheduler.scheduleAtFixedRate(() -> {
-
-            synchronized (this.emailLogs) {
-
-                Iterator<Map.Entry<String, UserEmailLog>> iterator = this.emailLogs.entrySet().iterator();
-
-                System.out.println("@@@ Running Clean Scheduler " + this.emailLogs.size());
-
-                // Iterate through the HashMap and remove entries containing "Remove"
-                while (iterator.hasNext()) {
-
-                    UserEmailLog eLog = iterator.next().getValue();
-                    long result = System.currentTimeMillis() - eLog.getEmailSentDateTime();
-
-                    if (ruleType == RuleType.ByMinute && (result > Constants.MINUTE_IN_MILLISECONDS)) {
-                        System.out.println("### ByMinute - Message Cleaned");
-                        iterator.remove();
-                    } else if (ruleType == RuleType.ByHour && (result > Constants.HOUR_IN_MILLISECONDS)) {
-                        System.out.println("### ByHour - Message Cleaned");
-                        iterator.remove();
-                    } else if (ruleType == RuleType.ByDay && (LocalDate.now().toEpochDay() > eLog.getEmailSentDateTime())) {
-                        System.out.println("### ByDay - Message Cleaned");
-                        iterator.remove();
-                    }
-
-                }
-            }
-
-        }, 0, Constants.SCHEDULER_WAIT, translateToTimeUnit(ruleType));
+    private boolean IsRuleByMinute() {
+        return this.ruleType == RuleType.ByMinute;
     }
 
-    @Override
-    public boolean CanSendEmail(String userId) {
-
-        UserEmailLog eLog = this.emailLogs.get(userId);
-        if (eLog == null) {
-            return true;
-        }
-
-        switch (ruleType) {
-            case ByMinute:
-                if ((System.currentTimeMillis() - eLog.getEmailSentDateTime()) < Constants.MINUTE_IN_MILLISECONDS && eLog.getEmailCounter() < emailThreshold) {
-                    return true;
-                }
-                break;
-            case ByHour:
-                if ((System.currentTimeMillis() - eLog.getEmailSentDateTime()) < Constants.HOUR_IN_MILLISECONDS && eLog.getEmailCounter() < emailThreshold) {
-                    return true;
-                }
-                break;
-            case ByDay:
-                if ((LocalDate.now().toEpochDay() > eLog.getEmailSentDateTime()) && eLog.getEmailCounter() < emailThreshold) {
-                    return true;
-                }
-                break;
-        }
-
-        return false;
+    private boolean IsRuleByHour() {
+        return this.ruleType == RuleType.ByHour;
     }
 
-    @Override
-    public void RegisterEmailSent(String userId) {
-        long emailSentAt = ruleType == RuleType.ByDay ? LocalDate.now().toEpochDay() : System.currentTimeMillis();
-        UserEmailLog eLog = this.emailLogs.get(userId);
-        if (eLog == null) {
-            eLog = new UserEmailLog(emailSentAt);
-            this.emailLogs.put(userId, eLog);
-        } else {
-            eLog.update(emailSentAt);
-        }
+    private boolean IsRuleByDay() {
+        return this.ruleType == RuleType.ByDay;
     }
+
 
 }
